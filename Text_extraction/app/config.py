8958679f87400b8aee135db93beebfde21d00b6e
madhurkgp <madhurkgp@users.noreply.github.com>
2026-01-14 10:07:34 +0530

    web page of text extracter

---
+++ Text_extraction/app/config.py
@@ -0,0 +1,23 @@
+import os
+from os import environ
+
+class Config(object):
+
+    DEBUG = False
+    TESTING = False
+
+    basedir = os.path.abspath(os.path.dirname(__file__))
+
+    SECRET_KEY = 'text_extraction_app'
+
+    UPLOADS = os.path.join(basedir, 'app', 'static', 'uploads')
+
+    SESSION_COOKIE_SECURE = True
+    DEFAULT_THEME = None
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SESSION_COOKIE_SECURE = False
+
+class DebugConfig(Config):
+    DEBUG = False
